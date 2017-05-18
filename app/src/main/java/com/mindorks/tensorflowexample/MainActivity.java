96d50d105f1695b19bec431ea48ed24affcb4139
Harsimar <simarsingh24@gmail.com>
2017-05-18 16:08:21 +0530

    added socket trigger

--- app/src/main/java/com/mindorks/tensorflowexample/MainActivity.java
+++ app/src/main/java/com/mindorks/tensorflowexample/MainActivity.java
@@ -1,39 +1,47 @@
-/*
- *    Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package com.mindorks.tensorflowexample;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.flurgle.camerakit.CameraListener;
 import com.flurgle.camerakit.CameraView;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static  String serverIP="192.168.1.39";
+    public static  int serverPort=8080;
+    private static final int PERMISSIONS_REQUEST = 1;
+
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    private static final String PERMISSION_INTERNET=Manifest.permission.INTERNET;
 
     private static final int INPUT_SIZE = 224;
     private static final int IMAGE_MEAN = 117;
@@ -56,6 +64,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (!hasPermission()) {
+            requestPermission();
+        }
+
         cameraView = (CameraView) findViewById(R.id.cameraView);
         imageViewResult = (ImageView) findViewById(R.id.imageViewResult);
         textViewResult = (TextView) findViewById(R.id.textViewResult);
@@ -95,9 +108,50 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Thread thread =new Thread(new Runnable() {
+            @Override
+            public void run() {
+                checkForTrigger();
+            }
+        });
+        thread.start();
+
         initTensorFlowAndLoadModel();
     }
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED
+                    && checkSelfPermission(PERMISSION_INTERNET) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
 
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA)
+                    || shouldShowRequestPermissionRationale(PERMISSION_INTERNET)) {
+                Toast.makeText(MainActivity.this, "Camera AND Internet permission" +
+                        " are required for this demo", Toast.LENGTH_LONG).show();
+            }
+            requestPermissions(new String[] {PERMISSION_CAMERA, PERMISSION_INTERNET}, PERMISSIONS_REQUEST);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    //ToDo---------------
+
+                } else {
+                    requestPermission();
+                }
+            }
+        }
+    }
     @Override
     protected void onResume() {
         super.onResume();
@@ -119,6 +173,87 @@ public class MainActivity extends AppCompatActivity {
                 classifier.close();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(item.getItemId()==R.id.connect){
+
+            View view= LayoutInflater.from(MainActivity.this).inflate(R.layout.alert_connection,null);
+            final EditText ipEditText=(EditText)view.findViewById(R.id.connection_ip_editText);
+            final EditText portEditText=(EditText)view.findViewById(R.id.connection_port_editText);
+
+            AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this)
+                    .setMessage("Enter connection details: ")
+                    .setView(view)
+                    .setPositiveButton("Connect", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            Toast.makeText(MainActivity.this,"Connected"
+                                    ,Toast.LENGTH_SHORT).show();
+
+                            serverIP=ipEditText.getText().toString();
+                            serverPort=Integer.parseInt(portEditText.getText().toString());
+
+                        }
+                    }).setNegativeButton("Cancel",null)
+                    .setCancelable(false);
+            AlertDialog alert=builder.create();
+            alert.show();
+
+        }
+
+        return true;
+    }
+
+    private void checkForTrigger() {
+        String msg_received;
+        while(true){
+            try{
+                ServerSocket socket=new ServerSocket(serverPort);
+                Socket clientSocket=socket.accept();
+                DataInputStream DIS = new DataInputStream(clientSocket.getInputStream());
+                msg_received = DIS.readUTF();
+                clientSocket.close();
+                socket.close();
+            }
+            catch(IOException e){
+                msg_received=e.toString();
+            }
+            if(msg_received.equals("transmit_data")){
+
+                Log.d("harsimarSingh","triggered");
+
+                sendData("harsimar*");
+            }}
+
+    }
+
+    public void sendData (final String s){
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Socket socket=new Socket(serverIP,serverPort);
+                    DataOutputStream DOS= new DataOutputStream(
+                            socket.getOutputStream());
+                    DOS.writeUTF(s);
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
     }
 
     private void initTensorFlowAndLoadModel() {
@@ -150,5 +285,7 @@ public class MainActivity extends AppCompatActivity {
                 btnDetectObject.setVisibility(View.VISIBLE);
             }
         });
+
+
     }
 }
--- app/src/main/java/com/mindorks/tensorflowexample/TensorFlowImageClassifier.java
+++ app/src/main/java/com/mindorks/tensorflowexample/TensorFlowImageClassifier.java
@@ -1,19 +1,3 @@
-/*
- *    Copyright (C) 2017 MINDORKS NEXTGEN PRIVATE LIMITED
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package com.mindorks.tensorflowexample;
 
 import android.content.res.AssetManager;
@@ -32,13 +16,6 @@ import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Vector;
 
-/**
- * Created by amitshekhar on 06/03/17.
- */
-
-/**
- * A classifier specialized to label images using TensorFlow.
- */
 public class TensorFlowImageClassifier implements Classifier {
 
     private static final String TAG = "TensorFlowImageClassifier";
